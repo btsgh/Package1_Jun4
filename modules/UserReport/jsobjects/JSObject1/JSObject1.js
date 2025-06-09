0b33f7b9fc252a1d72f549ddb33e824a1bc39db5
bts <aparnar81@gmail.com>
2025-06-09 08:22:48 +0000

    new commit with a jsmodule added

---
+++ modules/UserReport/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,15 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		
+		
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async	Button1onClick () {
+		return await Find_movies1.run({collection: 'movies'}) 
+		//	write code here
+	}
+}
+
